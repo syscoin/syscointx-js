c9e3c06cc013fd0e9e67473c1089abfdf4815b92
sidhujag <jsidhu@blockchainfoundry.co>
2020-05-27 18:31:17 -0700

    add test fixture for activate

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -6,12 +6,12 @@ module.exports = [{
   version: utils.SYSCOIN_TX_VERSION_ASSET_ACTIVATE,
   feeRate: new BN(10),
   utxos: [
-    {txId: '58c6e8163247f7c3fe1f36762eb1d0e2bc9bd438a5cf1823ed57ffa90b1663e8', vout: 0, script: '0014005a9faa5bd4e40aa97e456fe177deea6ce7109a', value: new ext.BN(100000000000) },
+    {txId: '698890216173b8cd6d0b86b3c7c4e66a0641829bede3d66b4ba2fc905dc65ef1', vout: 1, script: '001424e9ca410d96a30f68b932af7664b8f06d8ca78b', value: new ext.BN(100000000000) },
   ],
   assetOpts: {precision: 8, contract: 'contractaddr', pubdata: 'publicvalue', symbol: 'CAT', updateflags: 31, prevcontract: '', prevpubdata: '', balance: new ext.BN(10000000000), maxsupply: new ext.BN(100000000000), },
-  sysChangeAddress: '',
+  sysChangeAddress: 'bcrt1qzxn3zdqnepcszwf8jejfdz9tsyqlx82h7mcsc4',
   expected: {
-    script: ''
+    script: '6a3301f15ec65d01010008001d7b226465736372697074696f6e223a227075626c696376616c7565227d034341541f00001f648668'
   }
 }
 ]
